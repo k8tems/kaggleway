ebb6bb662c24590a7275478beb1213ec548a7594
k8tems <k8tems@live.com>
2024-04-16 18:15:54 +0900

    Minor refactor

--- ps4e4.py
+++ ps4e4.py
@@ -13,15 +13,13 @@ def process_feats(df):
 
 def get_train_df():
     train_df = pd.read_csv(COMP_ROOT / 'train.csv')
-    train_ids = train_df['id']
-    train_x_df = train_df.drop(['id', target_col], axis=1)
-    train_y_df = train_df[target_col]
-    process_feats(train_x_df)
-    return train_x_df, train_y_df, train_ids
+    train_ids = train_df.pop('id', axis=1)
+    train_y_df = train_df.pop(target_col, axis=1)
+    process_feats(train_df)
+    return train_df, train_y_df, train_ids
 
 
 def get_test_df():
     test_df = pd.read_csv(COMP_ROOT / 'test.csv')
-    test_ids = test_df['id']
     process_feats(test_df)
-    return test_df.drop('id', axis=1), test_ids
+    return test_df, test_df.pop('id', axis=1)
