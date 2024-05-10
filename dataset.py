2501b587028a5bf6b6bde0069544a23deadbabe3
k8tem <k8tems@live.com>
2024-05-10 18:13:13 +0900

    Implement unzip using python so that I can run the script in Windows

--- dataset.py
+++ dataset.py
@@ -1,7 +1,8 @@
-import sys
+import zipfile
 import csv
 import subprocess
 import argparse
+from pathlib import Path
 
 
 def load_csv(f_name):
@@ -18,11 +19,16 @@ def run_cmd(cmd):
         return None
 
 
+def unzip_file(src, dst):
+    with zipfile.ZipFile(src, 'r') as zip_ref:
+        zip_ref.extractall(dst)
+
+
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('csv_path', type=str, default='dataset.csv',
+    parser.add_argument('csv_path', type=Path, default='dataset.csv',
                         help='Location of csv file containing the list of datasets to download')
-    parser.add_argument('dst_path', type=str, default='/kaggle/input',
+    parser.add_argument('dst_path', type=Path, default='/kaggle/input',
                         help='Directory where the unzipped folder will be saved at')
     return parser.parse_args()
 
@@ -38,4 +44,5 @@ if __name__ == '__main__':
             cmd = 'kaggle datasets download'
             d_name = name.split('/')[-1]
         run_cmd(f'{cmd} {name} -p {args.dst_path}')
-        run_cmd(f'unzip -nd {d_name} {d_name}.zip')
+        dst = args.dst_path / d_name
+        unzip_file(str(dst) + '.zip', dst)
