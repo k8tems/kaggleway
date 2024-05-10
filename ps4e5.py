c2ca8bb87d439139549c62d363910a9ff404854c
k8tem <k8tems@live.com>
2024-05-10 17:30:37 +0900

    Remove relative imports from utils.py and add class that manages DataFrames of the current competition

--- ps4e5.py
+++ ps4e5.py
@@ -1,11 +1,34 @@
 import pandas as pd
-from .utils import KAGGLE_ROOT, submit, stage_df
+from utils import KAGGLE_ROOT, submit, stage_df
 from pathlib import Path
 
 
 target_col = 'FloodProbability'
 COMP_ROOT = KAGGLE_ROOT / 'playground-series-s4e5'
 SUBMISSION_DIR = Path('/notebooks/ps4e5/submissions')
+
+
+class Competition:
+    def __init__(self, name: str, kaggle_root: Path = None):
+        self.comp_root = (Path(kaggle_root) or Path('/kaggle/input')) / name
+
+    def get_train_df(self):
+        raise NotImplementedError()
+
+    def get_test_df(self):
+        raise NotImplementedError()
+
+
+class PS4E5(Competition):
+    def __init__(self, kaggle_root: Path = None):
+        super(PS4E5, self).__init__('playground-series-s4e5', kaggle_root=kaggle_root)
+
+    def get_train_df(self):
+        train_df = pd.read_csv(self.comp_root / 'train.csv')
+        train_ids = train_df.pop('id')
+        train_y_df = train_df.pop(target_col)
+        process_feats(train_df)
+        return train_df, train_y_df, train_ids
 
 
 def process_feats(df):
