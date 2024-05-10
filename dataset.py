ff5f3e69349927c03f30271490b46d7f0e01e3db
k8tem <k8tems@live.com>
2024-05-10 18:04:43 +0900

    Use argparse instead of sys.argv and set default values to the arguments

--- dataset.py
+++ dataset.py
@@ -1,6 +1,7 @@
 import sys
 import csv
 import subprocess
+import argparse
 
 
 def load_csv(f_name):
@@ -17,9 +18,18 @@ def run_cmd(cmd):
         return None
 
 
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('csv_path', type=str, default='dataset.csv',
+                        help='Location of csv file containing the list of datasets to download')
+    parser.add_argument('dst_path', type=str, default='/kaggle/input',
+                        help='Directory where the unzipped folder will be saved at')
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
-    csv_data = load_csv(sys.argv[1])
-    dst = sys.argv[2]
+    args = get_args()
+    csv_data = load_csv(args.csv_path)
     for name, data_type in csv_data:
         if data_type == 'c':
             cmd = 'kaggle competitions download -c'
@@ -27,5 +37,5 @@ if __name__ == '__main__':
         else:
             cmd = 'kaggle datasets download'
             d_name = name.split('/')[-1]
-        run_cmd(f'{cmd} {name} -p {dst}')
+        run_cmd(f'{cmd} {name} -p {args.dst_path}')
         run_cmd(f'unzip -nd {d_name} {d_name}.zip')
