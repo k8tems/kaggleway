9cdc42355f8e0a602d60a567f05cd0d8365b9fbb
k8tem <k8tems@live.com>
2024-05-10 17:42:21 +0900

    DRY csv file reading function

--- ps4e5.py
+++ ps4e5.py
@@ -25,15 +25,18 @@ class PS4E5(Competition):
     def __init__(self, kaggle_root: Path = None):
         super(PS4E5, self).__init__('playground-series-s4e5', kaggle_root=kaggle_root)
 
+    def read_csv(self, f_name):
+        return pd.read_csv(self.comp_root / f_name)
+
     def get_train_df(self):
-        train_df = pd.read_csv(self.comp_root / 'train.csv')
+        train_df = self.read_csv('train.csv')
         train_ids = train_df.pop('id')
         train_y_df = train_df.pop(target_col)
         process_feats(train_df)
         return train_df, train_y_df, train_ids
 
     def get_test_df(self):
-        test_df = pd.read_csv(self.comp_root / 'test.csv')
+        test_df = self.read_csv('test.csv')
         process_feats(test_df)
         return test_df, test_df.pop('id')
 
